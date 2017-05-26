973fbc90d25b4f04be750d9e63dc1b4880d39488
Jack Hoffman <jackhoff34@gmail.com>
2017-05-25 21:04:30 -0400

    bump
    put stuff into 1 method

--- Ball Game/Assets/Movement.cs
+++ Ball Game/Assets/Movement.cs
@@ -5,6 +5,15 @@ public class Movement : MonoBehaviour {
 
     public Rigidbody rb;
 
+    public Material material;
+    public float jumpPower = 5.0f;
+
+    private Vector3 physics;
+
+    public bool jump;
+    public bool quick;
+    public bool invert;
+
     public static float speed;
 
     public static float orignalSpeed;
@@ -13,10 +22,77 @@ public class Movement : MonoBehaviour {
 	void Start () {
         rb = GetComponent<Rigidbody>();
         orignalSpeed = speed;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+        jump = false;
+        quick = false;
+        invert = false;
+        physics = Physics.gravity;
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            jump = !jump;
+            quick = false;
+            invert = false;
+
+            if (jump)
+                material.SetColor("_red", Color.red);
+            else
+                material.SetColor("_white", Color.white);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            quick = !quick;
+            invert = false;
+            jump = false;
+
+            if (quick)
+            {
+                setSpeed((float)(Movement.getSpeed()) * 2.0f);
+                material.SetColor("_green", Color.green);
+            }
+            else
+                material.SetColor("_white", Color.white);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            invert = !invert;
+            quick = false;
+            jump = false;
+
+            if (quick)
+                material.SetColor("_blue", Color.blue);
+            else
+                material.SetColor("_white", Color.white);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jump)
+                rb.AddForce(new Vector3(0, jumpPower, 0));
+            else if (invert)
+                Physics.gravity *= -1;
+        }
+
+        if (!quick)
+            setSpeed(getOriginalSpeed());
+        if (!invert)
+            Physics.gravity = physics;
+
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
